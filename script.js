7d7d0ef2a9758290a8936cb881dc39e25ae744c6
Jldhv <dohova.julia201463@gmail.com>
2023-07-06 17:20:16 +0300

    my changes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+alert (`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
+
+minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
+maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue < -999) ? maxValue = -999 : maxValue;
+if (maxValue < minValue) {
+    [maxValue, minValue] = [minValue, maxValue]; // Значения меняются местами если max меньше min.
+}
+if (Number.isNaN(maxValue)) {
+    minValue = 0;
+    maxValue = 100;
+}
 
 const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
@@ -11,11 +21,38 @@ const answerField = document.getElementById('answerField');
 orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
+
+let units = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+let teens = ['', 'десять', 'одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+let dozens = ['', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+function numberToText() { 
+    let number = Math.abs(answerNumber);
+    let text = '';
+
+    if (number == 0) {
+        text = 'ноль';
+        return text;
+    }
+
+    if (number <= 9) {
+        return units[Math.floor(Math.abs(number) / 1)];
+    }
+
+    if (number > 9 && number < 20) {
+        return teens[Math.floor(number / 10 + number % 10)];
+    }
+
+    if (number >= 20 && number <= 99) {
+        return dozens[(Math.floor(number / 10)) - 1] + " " + units[Math.floor(number % 10)];
+    }
+
+}
+
+
 document.getElementById('btnRetry').addEventListener('click', function () {
     minValue = 0;
     maxValue = 100;
     orderNumber = 0;
-    
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
@@ -38,10 +75,11 @@ document.getElementById('btnOver').addEventListener('click', function () {
         }
     }
 })
+
 document.getElementById('btnLess').addEventListener('click', function () {
     if (gameRun){
-        if (maxValue === minValue){
-            const phraseRandom = Math.round( Math.random());
+        if (minValue === maxValue|| minValue == answerNumber){
+            const phraseRandom = Math.round( Math.random()*3);
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Вы забыли какое число загадали?`;
@@ -51,10 +89,12 @@ document.getElementById('btnLess').addEventListener('click', function () {
             gameRun = false;
         } else {
             maxValue = answerNumber  - 1;
-            answerNumber  = Math.floor((maxValue - minValue) / 2);
-            orderNumber--;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
             orderNumberField.innerText = orderNumber;
             answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?` : numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
+        
         }
     }
 })
